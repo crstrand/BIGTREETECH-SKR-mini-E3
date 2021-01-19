7420f05d7235eb00b73a2a9e1d6e4893ae22e330
Cameron Strandlund <crs@shaw.ca>
2021-01-19 08:02:16 -0700

    Use .../Marlin/Version.h (vendor version.h) for custom distribution date instead of /src/inc/Version.h

--- firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Version.h
+++ firmware/V1.2/Marlin-2.0.7.2-SKR-mini-E3-V1.2/Marlin/Version.h
@@ -21,56 +21,102 @@
  */
 #pragma once
 
-////////////////////////////
-// VENDOR VERSION EXAMPLE //
-////////////////////////////
+/**
+ * Release version. Leave the Marlin version or apply a custom scheme.
+ */
+#ifndef SHORT_BUILD_VERSION
+  #define SHORT_BUILD_VERSION "2.0.7.2"
+#endif
 
 /**
- * Marlin release version identifier
+ * Verbose version identifier containing a unique identifier, such as the
+ * vendor name, download location, GitHub account, etc.
  */
-//#define SHORT_BUILD_VERSION "2.0.7.2"
-
-/**
- * Verbose version identifier which should contain a reference to the location
- * from where the binary was downloaded or the source code was compiled.
- */
-//#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+#ifndef DETAILED_BUILD_VERSION
+  #define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION
+#endif
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2020-07-09"
+#ifndef STRING_DISTRIBUTION_DATE
+  #define STRING_DISTRIBUTION_DATE __DATE__" " __TIME__ //"2020-10-15"
+#endif
 
 /**
- * Defines a generic printer name to be output to the LCD after booting Marlin.
+ * Minimum Configuration.h and Configuration_adv.h file versions.
+ * Set based on the release version number. Used to catch an attempt to use
+ * older configurations. Override these if using a custom versioning scheme
+ * to alert users to major changes.
  */
-//#define MACHINE_NAME "3D Printer"
+
+#define MARLIN_HEX_VERSION 020007
+#ifndef REQUIRED_CONFIGURATION_H_VERSION
+  #define REQUIRED_CONFIGURATION_H_VERSION MARLIN_HEX_VERSION
+#endif
+#ifndef REQUIRED_CONFIGURATION_ADV_H_VERSION
+  #define REQUIRED_CONFIGURATION_ADV_H_VERSION MARLIN_HEX_VERSION
+#endif
 
 /**
- * The SOURCE_CODE_URL is the location where users will find the Marlin Source
- * Code which is installed on the device. In most cases —unless the manufacturer
- * has a distinct Github fork— the Source Code URL should just be the main
- * Marlin repository.
+ * The protocol for communication to the host. Protocol indicates communication
+ * standards such as the use of ASCII, "echo:" and "error:" line prefixes, etc.
+ * (Other behaviors are given by the firmware version and capabilities report.)
  */
-//#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#ifndef PROTOCOL_VERSION
+  #define PROTOCOL_VERSION "1.0"
+#endif
+
+/**
+ * Define a generic printer name to be output to the LCD after booting Marlin.
+ */
+#ifndef MACHINE_NAME
+  #define MACHINE_NAME "3D Printer"
+#endif
+
+/**
+ * Website where users can find Marlin source code for the binary installed on the
+ * device. Override this if you provide public source code download. (GPLv3 requires
+ * providing the source code to your customers.)
+ */
+#ifndef SOURCE_CODE_URL
+  #define SOURCE_CODE_URL "https://github.com/bigtreetech/BIGTREETECH-SKR-mini-E3"
+#endif
 
 /**
  * Default generic printer UUID.
  */
-//#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+#ifndef DEFAULT_MACHINE_UUID
+  #define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+#endif
 
-/**
- * The WEBSITE_URL is the location where users can get more information such as
- * documentation about a specific Marlin release.
- */
-//#define WEBSITE_URL "https://marlinfw.org"
+  /**
+   * The WEBSITE_URL is the location where users can get more information such as
+   * documentation about a specific Marlin release. Displayed in the Info Menu.
+   */
+#ifndef WEBSITE_URL
+  #define WEBSITE_URL "https://marlinfw.org"
+#endif
 
 /**
  * Set the vendor info the serial USB interface, if changable
  * Currently only supported by DUE platform
  */
-//#define USB_DEVICE_VENDOR_ID           0x0000
-//#define USB_DEVICE_PRODUCT_ID          0x0000
-//#define USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
+#ifndef USB_DEVICE_VENDOR_ID
+  #define  USB_DEVICE_VENDOR_ID           0x03EB /* ATMEL VID */
+#endif
+#ifndef USB_DEVICE_PRODUCT_ID
+  #define  USB_DEVICE_PRODUCT_ID          0x2424 /* MSC / CDC */
+#endif
+//! USB Device string definitions (Optional)
+#ifndef USB_DEVICE_MANUFACTURE_NAME
+  #define  USB_DEVICE_MANUFACTURE_NAME    WEBSITE_URL
+#endif
+#ifdef CUSTOM_MACHINE_NAME
+  #define  USB_DEVICE_PRODUCT_NAME        CUSTOM_MACHINE_NAME
+#else
+  #define  USB_DEVICE_PRODUCT_NAME        MACHINE_NAME
+#endif
+#define  USB_DEVICE_SERIAL_NAME           "123985739853"
